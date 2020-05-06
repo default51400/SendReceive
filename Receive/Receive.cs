212ea21f546181896a0b42576e1713c0a91b2424
default51400WNB <31726540+default51400@users.noreply.github.com>
2020-05-06 22:44:54 +0300

    Initial commit

---
+++ Receive/Receive.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() 
+            {
+                HostName = "localhost",
+                UserName = "root",
+                Password = "root"
+
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "CashService",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.Span);
+                        Console.WriteLine($"[x] received {message}");
+                    };
+
+                    channel.BasicConsume(queue: "CashService",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine("Pr key to exit");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace Send
+{
+    class Human
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "root",
+                Password = "root"
+
+            };
+
+            Human hum = new Human { Name = "Alex", Age = 34 };
+            string json = JsonSerializer.Serialize<Human>(hum);
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "CashService",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    var body = Encoding.UTF8.GetBytes(json);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "CashService",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", json);
+                }
+            }
+            
+            Console.WriteLine("Pr key to exit");
+            Console.ReadLine();
+        }
+    }
+}
